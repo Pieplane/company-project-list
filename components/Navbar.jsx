36c7705a49f9b29dfe3a27a6f036a2808e7ebc33
Pieplane <smolkin_97@inbox.ru>
2024-10-10 23:44:11 +0700

    Was improved navbar desktop and created navbar mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,34 +1,75 @@
+'use client'
 import React from 'react'
 import '../assets/styles/navbar.css';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
-    <div className="navbar">
-        <img src="images/_2834812530736.png" className="cyberia"/>
+    <nav>
+        <div className="desktop-menu">
+            <div className="desktop-page">
+            <div className="navbar-desktop">
+                <img src="images/_2834812530736.png" className="desktop-cyberia"/>
+                    <div className="nav-desktop-item agency">
+                        Агенство
+                    </div>
 
-        <div className="group128">
-            <div className="agency">
-                Агенство
+                    <div className="nav-desktop-item services">
+                        Услуги
+                    </div>
+
+                    <div className="nav-desktop-item cases">
+                        <Link href='/cases'>Кейсы</Link>
+                    </div>
+
+                    <div className="nav-desktop-item blog">
+                        Блог
+                    </div>
+
+                    <div className="nav-desktop-item contacts">
+                        Контакты
+                    </div>
             </div>
-
-            <div className="services">
-                Услуги
-            </div>
-
-            <div className="cases">
-                <Link href='/cases'>Кейсы</Link>
-            </div>
-
-            <div className="blog">
-                Блог
-            </div>
-
-            <div className="contacts">
-                Контакты
             </div>
         </div>
-    </div>
+
+        <div className="mobile-menu">
+            <div className="mobile-page">
+                <div className="navbar-mobile">
+                    <div className="mobile-menu-burger" onClick={() => setIsMobileMenuOpen((prev) => !prev)} style={{cursor: 'pointer'}}>
+                        <img src="images/Menu-burger.png"/>
+                    </div>
+                    <img src="images/_2834812530736.png" className="mobile-cyberia"/>
+                </div>
+                {isMobileMenuOpen && (<div className="nav-mobile-burger">
+                    <div className="nav-mobile-item agency">
+                        Агенство
+                    </div>
+
+                    <div className="nav-mobile-item services">
+                        Услуги
+                    </div>
+
+                    <div className="nav-mobile-item scases">
+                        Кейсы
+                    </div>
+
+                    <div className="nav-mobile-item blog">
+                        Блог
+                    </div>
+
+                    <div className="nav-mobile-item contacts">
+                        Контакты
+                    </div>
+                </div>)
+                }
+                </div>
+            </div>
+    </nav>
+    
   )
 }
 
